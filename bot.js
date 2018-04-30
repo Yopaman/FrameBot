d84f3ad6d0e603f72323af98df43005ff075cd9a
Yopaman <pablo99@free.fr>
2018-05-01 00:32:41 +0200

    System to avoid rate limit

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Jimp = require("jimp")
 const Twit = require("twit")
 const fs = require('fs')
 const config = require("./config.json")
+let tweetsNbr = 0
 
 const T = new Twit({
     consumer_key:         config.tw_consumer_key,
@@ -14,32 +15,39 @@ let stream = T.stream('statuses/filter', {track: '@' + config.tw_user})
 
 stream.on("tweet", tweet => {
     if (tweet.entities.media != null) {
-        let frame = Jimp.read("pictures/frame.png"),
-        background = Jimp.read(tweet.entities.media[0].media_url)
+        if (tweetsNbr < 16) {
+            let frame = Jimp.read("pictures/frame.png"),
+            background = Jimp.read(tweet.entities.media[0].media_url)
 
-        Promise.all([frame, background]).then(images => {
-            images[0].resize(images[1].bitmap.width, images[1].bitmap.height)
-            images[1].composite(images[0], 0, 0,)
-            .write("framed.png", (err, imgB64) => {
-                var b64content = fs.readFileSync('framed.png', { encoding: 'base64' })
-                T.post("media/upload", {media_data: b64content}, (err, data, res) => {
-                    console.log(data)
-                    let mediaIdStr = data.media_id_string
-                    let altText = "Awesome framed picture"
-                    let meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+            Promise.all([frame, background]).then(images => {
+                images[0].resize(images[1].bitmap.width, images[1].bitmap.height)
+                images[1].composite(images[0], 0, 0,)
+                .write("framed.png", (err, imgB64) => {
+                    var b64content = fs.readFileSync('framed.png', { encoding: 'base64' })
+                    T.post("media/upload", {media_data: b64content}, (err, data, res) => {
+                        let mediaIdStr = data.media_id_string
+                        let altText = "Awesome framed picture"
+                        let meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
-                    T.post('media/metadata/create', meta_params, (err, data, response) => {
-                        let params = {in_reply_to_status_id: tweet.id_str, status: "@" + tweet.user.screen_name + " Voilà l'image encadrée !", media_ids: [mediaIdStr]}
+                        T.post('media/metadata/create', meta_params, (err, data, response) => {
+                            let params = {in_reply_to_status_id: tweet.id_str, status: "@" + tweet.user.screen_name + " Voilà l'image encadrée !", media_ids: [mediaIdStr]}
 
-                        T.post("statuses/update", params, (err, data, res) => {
-                            console.log("Reponse envoyée !")
+                            T.post("statuses/update", params, (err, data, res) => {
+                                console.log("Reponse envoyée à @" + tweet.user.screen_name + " !")
+                            })
                         })
                     })
                 })
             })
-        })
+        } else {
+            console.log("Rate limit atteinte !")
+        }
     }
 })
+
+setInterval(() => {
+    tweetsNbr = 0
+}, 900000)
 
 
 
